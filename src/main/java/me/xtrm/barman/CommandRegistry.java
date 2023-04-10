7107627343730e45c330455818f30a52b7b3f4b9
xtrm <oss@xtrm.me>
2023-04-10 20:32:41 +0200

    ✨ feature(guy forgor): da fuckin

--- src/main/java/me/xtrm/barman/CommandRegistry.java
+++ src/main/java/me/xtrm/barman/CommandRegistry.java
@@ -1,25 +1,24 @@
 package me.xtrm.barman;
 
 import com.mojang.brigadier.CommandDispatcher;
-import me.xtrm.barman.command.*;
-import net.minecraft.server.command.ServerCommandSource;
-
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import me.xtrm.barman.command.*;
+import net.minecraft.server.command.ServerCommandSource;
 
 /**
  * @author xtrm
  */
 public enum CommandRegistry {
-    INSTANCE;
+  INSTANCE;
 
-    private final Set<Supplier<Consumer<CommandDispatcher<ServerCommandSource>>>> commands = Set.of(
-            SpawnCommand::new,
-            ToggleEndCommand::new
-    );
+  private final Set<Supplier<Consumer<CommandDispatcher<ServerCommandSource>>>>
+      commands = Set.of(
+          // SpawnCommand::new,
+          ToggleEndCommand::new);
 
-    public void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        commands.forEach(supplier -> supplier.get().accept(dispatcher));
-    }
+  public void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+    commands.forEach(supplier -> supplier.get().accept(dispatcher));
+  }
 }
